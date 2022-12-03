cbbead00fb6364de8248d680d4dde6038def0681
khuerecak <khuerecak@gmail.com>
2022-12-03 17:59:57 -0600

    initial commit

---
+++ js/db.js
@@ -0,0 +1,42 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  onSnapshot,
+} from "https://www.gstatic.com/firebasejs/9.3.0/firebase-firestore.js";
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyC9mKvVjlu-QDt1JbRX-f7CUoDZedx0IYg",
+    authDomain: "progress-2c6b3.firebaseapp.com",
+    projectId: "progress-2c6b3",
+    storageBucket: "progress-2c6b3.appspot.com",
+    messagingSenderId: "774101896467",
+    appId: "1:774101896467:web:3a8cd98a5978cff5589ee6"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = getFirestore(app);
+
+  async function getProgressions(db) {
+    const progressionsCol = collection(db, "progressions");
+    const progressionsSnapshot = await getDocs(progressionsCol);
+    const progressionsList = progressionsSnapshot.docs.map((doc) => doc);
+    return progressionsList;
+}
+
+const unsub = onSnapshot(collection(db, "progressions"), (doc) => {
+    //   console.log(doc.docChanges());
+    doc.docChanges().forEach((change) => {
+      // console.log(change, change.doc.data(), change.doc.id);
+      if (change.type === "added") {
+        //Call render function in UI
+        renderTask(change.doc.data(), change.doc.id);
+      }
+      if (change.type === "removed") {
+        //do something
+      }
+    });
+  });
