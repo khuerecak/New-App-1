27ee40a6c4c1b7abad12ddd5fea92ca8e26a1af0
khuerecak <khuerecak@gmail.com>
2022-12-12 20:29:20 -0600

    changed html & css

--- js/db.js
+++ js/db.js
@@ -4,39 +4,68 @@ import {
   collection,
   getDocs,
   onSnapshot,
+  addDoc,
+  deleteDoc,
+  doc,
 } from "https://www.gstatic.com/firebasejs/9.3.0/firebase-firestore.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyC9mKvVjlu-QDt1JbRX-f7CUoDZedx0IYg",
-    authDomain: "progress-2c6b3.firebaseapp.com",
-    projectId: "progress-2c6b3",
-    storageBucket: "progress-2c6b3.appspot.com",
-    messagingSenderId: "774101896467",
-    appId: "1:774101896467:web:3a8cd98a5978cff5589ee6"
-  };
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  databaseURL: 'https://{project-id}.firebaseio.com',
+  apiKey: "AIzaSyC9mKvVjlu-QDt1JbRX-f7CUoDZedx0IYg",
+  authDomain: "progress-2c6b3.firebaseapp.com",
+  projectId: "progress-2c6b3",
+  storageBucket: "progress-2c6b3.appspot.com",
+  messagingSenderId: "774101896467",
+  appId: "1:774101896467:web:3a8cd98a5978cff5589ee6"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-  async function getProgressions(db) {
-    const progressionsCol = collection(db, "progressions");
-    const progressionsSnapshot = await getDocs(progressionsCol);
-    const progressionsList = progressionsSnapshot.docs.map((doc) => doc);
-    return progressionsList;
+const db = getFirestore(app);
+
+async function getProgressions(db) {
+  const progressionsCol = collection(db, "progressions");
+  const progressionsSnapshot = await getDocs(progressionsCol);
+  const progressionsList = progressionsSnapshot.docs.map((doc) => doc);
+  return progressionsList;
 }
 
 const unsub = onSnapshot(collection(db, "progressions"), (doc) => {
-    //   console.log(doc.docChanges());
-    doc.docChanges().forEach((change) => {
-      // console.log(change, change.doc.data(), change.doc.id);
-      if (change.type === "added") {
-        //Call render function in UI
-        renderTask(change.doc.data(), change.doc.id);
-      }
-      if (change.type === "removed") {
-        //do something
-      }
-    });
+  //   console.log(doc.docChanges());
+  doc.docChanges().forEach((change) => {
+    // console.log(change, change.doc.data(), change.doc.id);
+    if (change.type === "added") {
+      //Call render function in UI
+      renderProgression(change.doc.data(), change.doc.id);
+    }
+    if (change.type === "removed") {
+    //  removeProgression(change.doc.id);
+    }
   });
+});
+
+//new progression
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  addDoc(collection(db, "progressions"), {
+    title: form.title.value,
+    type: form.type.value,
+  }).catch((error) => console.log(error));
+  form.title.value = "";
+  form.type.value = "";
+});
+
+//delete a progression
+
+const progContainer = document.querySelector(".progressions");
+progContainer.addEventListener("click", (event) => {
+  if (event.target.tagName === "I") {
+    const id = event.target.getAttribute("data-id");
+    deleteDoc(doc(db, "progressions", id));
+  }
+});
+
--- js/ui.js
+++ js/ui.js
@@ -9,29 +9,29 @@ document.addEventListener("DOMContentLoaded", function () {
     M.Sidenav.init(forms, { edge: "left" });
   });
 
-const renderProgression = (data, id)=>{
-  const html = ` <div class="center">
-  <div class="col s12 m7">
-    <div class="card">
-      <div class="card-image">
-        <img src="img/hidden_message.png" class="responsive-img materialboxed" >
-         <span class="card-title">${data.title}</span>
-           </div>
-            <div class="card-content">
-              <p>I am a very simple card. I am good at containing small bits of information.
-                 I am convenient because I require little markup to use effectively.</p>
-            </div>
-      <div class="icons">
-          <i class="material-icons">play_circle_outline</i>
-          <i class="material-icons">edit</i>
-          <i class="material-icons">dashboard</i>
-          <i class="material-icons">share</i>
-          <i class="material-icons">more_vert</i>
-      </div>
-    </div>
-  </div>
-</div>`;
+//const renderProgression = (data, id)=>{
+//  const html = ` <div class="center">
+//  <div class="col s12 m7">
+//    <div class="card">
+//      <div class="card-image">
+//        <img src="img/hidden_message.png" class="responsive-img materialboxed" >
+//         <span class="card-title">${data.title}</span>
+//           </div>
+//            <div class="card-content">
+//              <p>I am a very simple card. I am good at containing small bits of information.
+//                 I am convenient because I require little markup to use effectively.</p>
+//           </div>
+//      <div class="icons">
+//          <i class="material-icons">play_circle_outline</i>
+//          <i class="material-icons">edit</i>
+//          <i class="material-icons">dashboard</i>
+//          <i class="material-icons">share</i>
+///          <i class="material-icons">more_vert</i>
+//      </div>
+//    </div>
+//  </div>
+//</div>`;
 
-progressions.innerHTML += html;
+//progressions.innerHTML += html;
 
-};
+//};
